84d66aacd0b1ef50acf44a4caee420b903e2ccd5
Furkan Özkan <furkanozkanr@gmail.com>
2026-07-30 11:09:08 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "kizilkaya-v10";
+const CACHE_NAME = "kizilkaya-v11";
 const ASSETS = [
   "./",
   "./index.html",
@@ -6,7 +6,8 @@ const ASSETS = [
   "./app.js",
   "./manifest.json",
   "./icon-192.png",
-  "./icon-512.png"
+  "./icon-512.png",
+  "./badge-192.png"
 ];
 
 self.addEventListener("install", (event) => {
